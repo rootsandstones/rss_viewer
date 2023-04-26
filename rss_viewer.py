7c9d4cf9b7183a33fc475a03c5541bb2c614d2d2
rootsandstones <johndoe@example.com>
2023-04-26 20:20:50 +0200

    modified:   rss_viewer.py

--- rss_viewer.py
+++ rss_viewer.py
@@ -3,21 +3,19 @@ import feedparser
 
 rss_exmpl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCjOl2AUblVmg2rA_cRgZkFg'
 
-
-st.title('Youtube RSS Viewer')
-
-
 def get_rss_feed():
     feed = feedparser.parse(rss_exmpl)
-    feed_title = feed.feed.title
-    label_title = tk.Label(root, text=str(feed_title))
-    label_title.pack()
+    st.header(feed.feed.title)
         
     for i in range(0, 5):
         title = feed.entries[i].title
         link = feed.entries[i].link
-        label_videotitle = tk.Label(root, text=str(title))
-        label_videotitle.pack()
-        label_link = tk.Label(root, text=str(link))
-        label_link.pack()
+        img_link = feed.entries[i].media_thumbnail
+        img_link = img_link[0]['url']
 
+        st.image(img_link) 
+        st.write("_"+ title + "_")
+        st.write(link)
+
+st.title('Youtube RSS Viewer')
+get_rss_feed()
