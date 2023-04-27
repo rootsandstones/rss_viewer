b973815517bc0e3e337f4c419f133bd440365e9f
rootsandstones <johndoe@example.com>
2023-04-27 19:13:19 +0200

    Add input field

--- rss_viewer.py
+++ rss_viewer.py
@@ -3,8 +3,8 @@ import feedparser
 
 rss_exmpl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCjOl2AUblVmg2rA_cRgZkFg'
 
-def get_rss_feed():
-    feed = feedparser.parse(rss_exmpl)
+def get_rss_feed(rss_input):
+    feed = feedparser.parse(rss_input)
     st.header(feed.feed.title)
         
     for i in range(0, 5):
@@ -13,9 +13,12 @@ def get_rss_feed():
         img_link = feed.entries[i].media_thumbnail
         img_link = img_link[0]['url']
 
-        st.image(img_link) 
+        st.image(img_link,) 
         st.write("_"+ title + "_")
         st.write(link)
 
 st.title('Youtube RSS Viewer')
-get_rss_feed()
+rss_link = st.text_input(label='Youtube RSS Link', value='https://www.youtube.com/feeds/videos.xml?channel_id=UCjOl2AUblVmg2rA_cRgZkFg')
+
+
+get_rss_feed(rss_link)
