e9dd2aabfd6215e5118d00508ca9c0d61b2e4243
rootsandstones <johndoe@example.com>
2023-04-27 19:49:02 +0200

    Add sqlite db

--- rss_viewer.py
+++ rss_viewer.py
@@ -1,7 +1,21 @@
 import streamlit as st
 import feedparser
+import os
+import sqlite3
 
 rss_exmpl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCjOl2AUblVmg2rA_cRgZkFg'
+
+if 'rss.db' in os.listdir():
+    st.write('DB found')
+    db = sqlite3.connect('rss.db')
+else:
+    st.write('DB not available')
+    open('rss.db', 'a')
+    db = sqlite3.connect('rss.db')
+
+dbpointer = db.cursor()
+dbpointer.execute("CREATE TABLE IF NOT EXISTS links (name TEXT, link TEXT)")
+#db.commit()
 
 def get_rss_feed(rss_input):
     feed = feedparser.parse(rss_input)
@@ -13,7 +27,7 @@ def get_rss_feed(rss_input):
         img_link = feed.entries[i].media_thumbnail
         img_link = img_link[0]['url']
 
-        st.image(img_link,) 
+        st.image(img_link) 
         st.write("_"+ title + "_")
         st.write(link)
 
